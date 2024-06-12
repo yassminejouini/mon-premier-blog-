a476712ccad2aad7cf41f59ff9707363bda1f808
Yassmine <jouini082001@gmail.com>
2024-06-12 10:54:34 +0100

    Modified templates to display posts from database.

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'de-ch'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'Europe/Berlin'
 
